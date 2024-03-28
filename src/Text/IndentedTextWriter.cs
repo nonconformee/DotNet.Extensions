25fd83d4b13921fbb4732e2f1a4a2be953fb41b2
Franziska Roten <franziska.roten@roteninformatik.ch>
2024-03-28 12:53:20 +0100

    "Finished IndentedTextWriter"

--- src/Text/IndentedTextWriter.cs
+++ src/Text/IndentedTextWriter.cs
@@ -30,11 +30,18 @@ public sealed class IndentedTextWriter : TextWriter
     /// <param name="indentEmptyLines"> Specifies whether empty lines are indented or not. The default value is false. </param>
     /// <param name="indentLevel"> Specifies the indentation level to start with The default value is 0. </param>
     /// <param name="identString"> The string which is used for indenting one level. The default value is a single space. If an empty string is provided, a single space will be used.</param>
-    /// <param name="newLine"> The string which is used for ending a line and going to the next line. The default value is null. If null or an empty string is provided, the base writers value is used is used. </param>
+    /// <param name="newLine"> The string which is used for ending a line and going to the next line. The default value is null. If null is provided, the base writers value is used. If an empty string is provided, <see cref="Environment.NewLine"/> is used. </param>
+    /// <exception cref="ArgumentException"><paramref name="newLine"/> can only be \n or \r\n. </exception>
     public IndentedTextWriter (TextWriter writer, bool keepOpen = false, bool indentEmptyLines = false, int indentLevel = 0, string identString = " ", string? newLine = null)
     {
+        if(!string.IsNullOrWhiteSpace(newLine) && (newLine != "\n") && (newLine != "\r\n"))
+        {
+            throw new ArgumentException("Invalid line terminator.", nameof(newLine));
+        }
+
+
         BaseWriter = writer;
-        writer.NewLine = string.IsNullOrWhiteSpace(newLine) ? writer.NewLine : newLine;
+        BaseWriter.NewLine = newLine is null ? BaseWriter.NewLine : (string.IsNullOrWhiteSpace(newLine) ? Environment.NewLine : newLine);
 
         KeepOpen = keepOpen;
         IndentEmptyLines = indentEmptyLines;
@@ -119,7 +126,7 @@ public sealed class IndentedTextWriter : TextWriter
     public int IndentLevel
     {
         get => this._indentLevel;
-        private set
+        set
         {
             if (value < 0)
             {
@@ -135,6 +142,7 @@ public sealed class IndentedTextWriter : TextWriter
     /// </summary>
     /// <value>true if this instance has been disposed, false otherwise.</value>
     public bool IsClosed { get; private set;}
+
     #endregion
 
 
@@ -150,7 +158,7 @@ public sealed class IndentedTextWriter : TextWriter
         }
     }
 
-    private string CreateObjectString (object value)
+    private string CreateObjectString (object? value)
     {
         if (value is null)
         {
@@ -167,6 +175,10 @@ public sealed class IndentedTextWriter : TextWriter
 
     private void WriteIndent ()
     {
+        // TODO Only one call to BaseWriter
+
+        VerifyNotClosed();
+        
         if (_indentPending)
         {
             for (int i1 = 0; i1 < IndentLevel; i1++)
@@ -180,11 +192,49 @@ public sealed class IndentedTextWriter : TextWriter
 
     private void WriteLineIndent ()
     {
+        // TODO Only one call to BaseWriter
+        
+        VerifyNotClosed();
+        
         if (_indentPending && IndentEmptyLines)
         {
             for (int i1 = 0; i1 < IndentLevel; i1++)
             {
                 BaseWriter.Write(IndentString);
+            }
+
+            _indentPending = false;
+        }
+    }
+
+    private async ValueTask WriteIndentAsync ()
+    {
+        // TODO Only one call to BaseWriter
+        
+        VerifyNotClosed();
+        
+        if (_indentPending)
+        {
+            for (int i1 = 0; i1 < IndentLevel; i1++)
+            {
+                await BaseWriter.WriteAsync(IndentString);
+            }
+
+            _indentPending = false;
+        }
+    }
+
+    private async ValueTask WriteLineIndentAsync ()
+    {
+        // TODO Only one call to BaseWriter
+        
+        VerifyNotClosed();
+        
+        if (_indentPending && IndentEmptyLines)
+        {
+            for (int i1 = 0; i1 < IndentLevel; i1++)
+            {
+                await BaseWriter.WriteAsync(IndentString);
             }
 
             _indentPending = false;
@@ -237,412 +287,355 @@ public sealed class IndentedTextWriter : TextWriter
     public override void Close() => Dispose();
 
     /// <inheritdoc />
-    public override void Flush() => BaseWriter.Flush();
-
-    /// <inheritdoc />
     public override void Write (bool value)
     {
-        this.VerifyNotClosed();
-        this.WriteIndent();
-        this.BaseWriter.Write(value);
+        WriteIndent();
+        BaseWriter.Write(value);
     }
 
     /// <inheritdoc />
     public override void Write (char value)
     {
-        this.VerifyNotClosed();
-
-        if (value == '\r')
-        {
-            return;
-        }
-
-        if ((value == '\n') || (value == this.NewLine[0]))
-        {
-            this.WriteLine();
-        }
-        else
-        {
-            this.WriteIndent();
-
-            this.BaseWriter.Write(value);
-        }
+        WriteIndent();
+        BaseWriter.Write(value);
     }
 
     /// <inheritdoc />
-    public override void Write (char[] buffer)
+    public override void Write (char[]? buffer)
     {
-        if (buffer == null)
-        {
-            throw new ArgumentNullException(nameof(buffer));
-        }
-
-        this.VerifyNotClosed();
-
-        foreach (char chr in buffer)
-        {
-            this.Write(chr);
-        }
+        WriteIndent();
+        BaseWriter.Write(buffer);
     }
 
     /// <inheritdoc />
     public override void Write (char[] buffer, int index, int count)
     {
-        if (buffer == null)
-        {
-            throw new ArgumentNullException(nameof(buffer));
-        }
-
-        if ((index < 0) || ((index >= buffer.Length) && (count > 0)))
-        {
-            throw new ArgumentOutOfRangeException(nameof(index));
-        }
-
-        if ((count < 0) || ((index + count) >= buffer.Length))
-        {
-            throw new ArgumentOutOfRangeException(nameof(count));
-        }
-
-        this.VerifyNotClosed();
-
-        for (int i1 = 0; i1 < count; i1++)
-        {
-            this.Write(buffer[i1 + index]);
-        }
+        WriteIndent();
+        BaseWriter.Write(buffer, index, count);
     }
 
     /// <inheritdoc />
     public override void Write (decimal value)
     {
-        this.VerifyNotClosed();
-        this.WriteIndent();
-        this.BaseWriter.Write(value);
+        WriteIndent();
+        BaseWriter.Write(value);
     }
 
     /// <inheritdoc />
     public override void Write (double value)
     {
-        this.VerifyNotClosed();
-        this.WriteIndent();
-        this.BaseWriter.Write(value);
+        WriteIndent();
+        BaseWriter.Write(value);
     }
 
     /// <inheritdoc />
     public override void Write (float value)
     {
-        this.VerifyNotClosed();
-        this.WriteIndent();
-        this.BaseWriter.Write(value);
+        WriteIndent();
+        BaseWriter.Write(value);
     }
 
     /// <inheritdoc />
     public override void Write (int value)
     {
-        this.VerifyNotClosed();
-        this.WriteIndent();
-        this.BaseWriter.Write(value);
+        WriteIndent();
+        BaseWriter.Write(value);
     }
 
     /// <inheritdoc />
     public override void Write (long value)
     {
-        this.VerifyNotClosed();
-        this.WriteIndent();
-        this.BaseWriter.Write(value);
+        WriteIndent();
+        BaseWriter.Write(value);
     }
 
     /// <inheritdoc />
-    public override void Write (object value)
+    public override void Write (object? value)
     {
-        this.VerifyNotClosed();
-
-        string str = this.CreateObjectString(value);
-
-        this.Write(str);
+        WriteIndent();
+        BaseWriter.Write(value);
     }
 
     /// <inheritdoc />
-    public override void Write (string format, object arg0)
+    public override void Write (string format, object? arg0)
     {
-        this.VerifyNotClosed();
-
-        string str = string.Format(this.FormatProvider, format, arg0);
-
-        this.Write(str);
+        WriteIndent();
+        BaseWriter.Write(format, arg0);
     }
 
     /// <inheritdoc />
-    public override void Write (string format, object arg0, object arg1)
+    public override void Write (string format, object? arg0, object? arg1)
     {
-        this.VerifyNotClosed();
-
-        string str = string.Format(this.FormatProvider, format, arg0, arg1);
-
-        this.Write(str);
+        WriteIndent();
+        BaseWriter.Write(format, arg0, arg1);
     }
 
     /// <inheritdoc />
-    public override void Write (string format, object arg0, object arg1, object arg2)
+    public override void Write (string format, object? arg0, object? arg1, object? arg2)
     {
-        this.VerifyNotClosed();
-
-        string str = string.Format(this.FormatProvider, format, arg0, arg1, arg2);
-
-        this.Write(str);
+        WriteIndent();
+        BaseWriter.Write(format, arg0, arg1, arg2);
     }
 
     /// <inheritdoc />
-    public override void Write (string format, params object[] arg)
+    public override void Write (string format, params object?[] arg)
     {
-        this.VerifyNotClosed();
-
-        string str = string.Format(this.FormatProvider, format, arg);
-
-        this.Write(str);
+        WriteIndent();
+        BaseWriter.Write(format, arg);
     }
 
     /// <inheritdoc />
-    public override void Write (string value)
+    public override void Write (string? value)
     {
-        if (value == null)
-        {
-            throw new ArgumentNullException(nameof(value));
-        }
-
-        this.VerifyNotClosed();
-
-        for (int i1 = 0; i1 < value.Length; i1++)
-        {
-            this.Write(value[i1]);
-        }
+        WriteIndent();
+        BaseWriter.Write(value);
     }
 
     /// <inheritdoc />
-    [CLSCompliant(false),]
     public override void Write (uint value)
     {
-        this.VerifyNotClosed();
-        this.WriteIndent();
-        this.BaseWriter.Write(value);
+        WriteIndent();
+        BaseWriter.Write(value);
     }
 
     /// <inheritdoc />
-    [CLSCompliant(false),]
     public override void Write (ulong value)
     {
-        this.VerifyNotClosed();
-        this.WriteIndent();
-        this.BaseWriter.Write(value);
+        WriteIndent();
+        BaseWriter.Write(value);
+    }
+
+    /// <inheritdoc />
+    public override void Write(ReadOnlySpan<char> buffer)
+    {
+        WriteIndent();
+        BaseWriter.Write(buffer);
+    }
+
+    /// <inheritdoc />
+    public override void Write(StringBuilder? value)
+    {
+        WriteIndent();
+        BaseWriter.Write(value);
     }
 
     /// <inheritdoc />
     public override void WriteLine ()
     {
-        this.VerifyNotClosed();
-
-        this.WriteLineIndent();
-
-        this.BaseWriter.WriteLine();
-
-        this._indentPending = true;
+        BaseWriter.WriteLine();
+        _indentPending = true;
+        WriteLineIndent();
     }
 
     /// <inheritdoc />
     public override void WriteLine (bool value)
     {
-        this.VerifyNotClosed();
-        this.WriteIndent();
-        this.BaseWriter.WriteLine(value);
-        this._indentPending = true;
+        Write(value);
+        WriteLine();
     }
 
     /// <inheritdoc />
     public override void WriteLine (char value)
     {
-        this.VerifyNotClosed();
-
-        this.Write(value);
-
-        this.WriteLine();
+        Write(value);
+        WriteLine();
     }
 
     /// <inheritdoc />
-    public override void WriteLine (char[] buffer)
+    public override void WriteLine (char[]? buffer)
     {
-        if (buffer == null)
-        {
-            throw new ArgumentNullException(nameof(buffer));
-        }
-
-        this.VerifyNotClosed();
-
-        for (int i1 = 0; i1 < buffer.Length; i1++)
-        {
-            this.Write(buffer[i1]);
-        }
-
-        this.WriteLine();
+        Write(buffer);
+        WriteLine();
     }
 
     /// <inheritdoc />
     public override void WriteLine (char[] buffer, int index, int count)
     {
-        if (buffer == null)
-        {
-            throw new ArgumentNullException(nameof(buffer));
-        }
-
-        if ((index < 0) || ((index >= buffer.Length) && (count > 0)))
-        {
-            throw new ArgumentOutOfRangeException(nameof(index));
-        }
-
-        if ((count < 0) || ((index + count) >= buffer.Length))
-        {
-            throw new ArgumentOutOfRangeException(nameof(count));
-        }
-
-        this.VerifyNotClosed();
-
-        for (int i1 = 0; i1 < count; i1++)
-        {
-            this.Write(buffer[i1 + index]);
-        }
-
-        this.WriteLine();
+        Write(buffer, index, count);
+        WriteLine();
     }
 
     /// <inheritdoc />
     public override void WriteLine (decimal value)
     {
-        this.VerifyNotClosed();
-        this.WriteIndent();
-        this.BaseWriter.WriteLine(value);
-        this._indentPending = true;
+        Write(value);
+        WriteLine();
     }
 
     /// <inheritdoc />
     public override void WriteLine (double value)
     {
-        this.VerifyNotClosed();
-        this.WriteIndent();
-        this.BaseWriter.WriteLine(value);
-        this._indentPending = true;
+        Write(value);
+        WriteLine();
     }
 
     /// <inheritdoc />
     public override void WriteLine (float value)
     {
-        this.VerifyNotClosed();
-        this.WriteIndent();
-        this.BaseWriter.WriteLine(value);
-        this._indentPending = true;
+        Write(value);
+        WriteLine();
     }
 
     /// <inheritdoc />
     public override void WriteLine (int value)
     {
-        this.VerifyNotClosed();
-        this.WriteIndent();
-        this.BaseWriter.WriteLine(value);
-        this._indentPending = true;
+        Write(value);
+        WriteLine();
     }
 
     /// <inheritdoc />
     public override void WriteLine (long value)
     {
-        this.VerifyNotClosed();
-        this.WriteIndent();
-        this.BaseWriter.WriteLine(value);
-        this._indentPending = true;
+        Write(value);
+        WriteLine();
     }
 
     /// <inheritdoc />
-    public override void WriteLine (object value)
+    public override void WriteLine (object? value)
     {
-        this.VerifyNotClosed();
-
-        string str = this.CreateObjectString(value);
-
-        this.WriteLine(str);
+        Write(value);
+        WriteLine();
     }
 
     /// <inheritdoc />
-    public override void WriteLine (string format, object arg0)
+    public override void WriteLine (string format, object? arg0)
     {
-        this.VerifyNotClosed();
-
-        string str = string.Format(this.FormatProvider, format, arg0);
-
-        this.WriteLine(str);
+        Write(format, arg0);
+        WriteLine();
     }
 
     /// <inheritdoc />
-    public override void WriteLine (string format, object arg0, object arg1)
+    public override void WriteLine (string format, object? arg0, object? arg1)
     {
-        this.VerifyNotClosed();
-
-        string str = string.Format(this.FormatProvider, format, arg0, arg1);
-
-        this.WriteLine(str);
+        Write(format, arg0, arg1);
+        WriteLine();
     }
 
     /// <inheritdoc />
-    public override void WriteLine (string format, object arg0, object arg1, object arg2)
+    public override void WriteLine (string format, object? arg0, object? arg1, object? arg2)
     {
-        this.VerifyNotClosed();
-
-        string str = string.Format(this.FormatProvider, format, arg0, arg1, arg2);
-
-        this.WriteLine(str);
+        Write(format, arg0, arg1, arg2);
+        WriteLine();
     }
 
     /// <inheritdoc />
-    public override void WriteLine (string format, params object[] arg)
+    public override void WriteLine (string format, params object?[] arg)
     {
-        this.VerifyNotClosed();
-
-        string str = string.Format(this.FormatProvider, format, arg);
-
-        this.WriteLine(str);
+        Write(format, arg);
+        WriteLine();
     }
 
     /// <inheritdoc />
-    public override void WriteLine (string value)
+    public override void WriteLine (string? value)
     {
-        if (value == null)
-        {
-            throw new ArgumentNullException(nameof(value));
-        }
-
-        this.VerifyNotClosed();
-
-        for (int i1 = 0; i1 < value.Length; i1++)
-        {
-            this.Write(value[i1]);
-        }
-
-        this.WriteLine();
+        Write(value);
+        WriteLine();
     }
 
     /// <inheritdoc />
-    [CLSCompliant(false),]
     public override void WriteLine (uint value)
     {
-        this.VerifyNotClosed();
-        this.WriteIndent();
-        this.BaseWriter.WriteLine(value);
-        this._indentPending = true;
+        Write(value);
+        WriteLine();
     }
 
     /// <inheritdoc />
-    [CLSCompliant(false),]
     public override void WriteLine (ulong value)
     {
-        this.VerifyNotClosed();
-        this.WriteIndent();
-        this.BaseWriter.WriteLine(value);
-        this._indentPending = true;
+        Write(value);
+        WriteLine();
+    }
+
+    /// <inheritdoc />
+    public override void WriteLine(ReadOnlySpan<char> buffer)
+    {
+        Write(buffer);
+        WriteLine();
+    }
+
+    /// <inheritdoc />
+    public override void WriteLine(StringBuilder? value)
+    {
+        Write(value);
+        WriteLine();
+    }
+
+    /// <inheritdoc />
+    public override async Task WriteAsync(ReadOnlyMemory<char> buffer, CancellationToken cancellationToken = default)
+    {
+        await WriteIndentAsync();
+        await BaseWriter.WriteAsync(buffer, cancellationToken);
+    }
+
+    /// <inheritdoc />
+    public override async Task WriteAsync(StringBuilder? value, CancellationToken cancellationToken = default)
+    {
+        await WriteIndentAsync();
+        await BaseWriter.WriteAsync(value, cancellationToken);
+    }
+
+    /// <inheritdoc />
+    public override async Task WriteAsync(char value)
+    {
+        await WriteIndentAsync();
+        await BaseWriter.WriteAsync(value);
+    }
+
+    /// <inheritdoc />
+    public override async Task WriteAsync(char[] buffer, int index, int count)
+    {
+        await WriteIndentAsync();
+        await BaseWriter.WriteAsync(buffer, index, count);
+    }
+
+    /// <inheritdoc />
+    public override async Task WriteAsync(string? value)
+    {
+        await WriteIndentAsync();
+        await BaseWriter.WriteAsync(value);
+    }
+
+    /// <inheritdoc />
+    public override async Task WriteLineAsync()
+    {
+        await BaseWriter.WriteLineAsync();
+        _indentPending = true;
+        await WriteLineIndentAsync();
+    }
+
+    /// <inheritdoc />
+    public override async Task WriteLineAsync(ReadOnlyMemory<char> buffer, CancellationToken cancellationToken = default)
+    {
+        await WriteAsync(buffer, cancellationToken);
+        await WriteLineAsync();
+    }
+
+    /// <inheritdoc />
+    public override async Task WriteLineAsync(StringBuilder? value, CancellationToken cancellationToken = default)
+    {
+        await WriteAsync(value, cancellationToken);
+        await WriteLineAsync();
+    }
+
+    /// <inheritdoc />
+    public override async Task WriteLineAsync(char value)
+    {
+        await WriteAsync(value);
+        await WriteLineAsync();
+    }
+
+    /// <inheritdoc />
+    public override async Task WriteLineAsync(char[] buffer, int index, int count)
+    {
+        await WriteAsync(buffer, index, count);
+        await WriteLineAsync();
+    }
+
+    /// <inheritdoc />
+    public override async Task WriteLineAsync(string? value)
+    {
+        await WriteAsync(value);
+        await WriteLineAsync();
     }
 
     #endregion
